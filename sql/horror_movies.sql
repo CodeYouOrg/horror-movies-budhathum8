276b2b2841e7ac6569ee277a10c34a6678b29da9
Raj Kumar Shrestha <shresthagraj@gmail.com>
2024-10-10 19:08:11 -0400

    update horror_movies

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,8 @@
 -- Add your SQL here
+SELECT id as Movie_ID, name as Movie_Title, imdb_rating as Rating
+FROM movies
+wHERE genre = 'horror'
+AND year <= 1985
+AND name IS NOT NULL
+order by imdb_rating desc
+LIMIT 3;
